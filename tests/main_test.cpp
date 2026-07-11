b329dcddf433b88a2c493b2a529d19a8a4ae898e
Marcus441 <88486170+Marcus441@users.noreply.github.com>
2026-07-11 14:36:34 +1000

    test(ringbuffer): add additional bool checks to get and put calls

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -5,18 +5,18 @@
 
 TEST(RingBufferTest, PutIncreasesSizeAndIsNotEmpty) {
   RingBuffer rb(10);
-  rb.Put(42);
+  EXPECT_TRUE(rb.Put(42));
   EXPECT_FALSE(rb.Empty());
 }
 
 TEST(RingBufferTest, GetReturnsInOrder) {
   RingBuffer rb(3);
-  rb.Put(1);
-  rb.Put(2);
+  ASSERT_TRUE(rb.Put(1));
+  ASSERT_TRUE(rb.Put(2));
   int value{};
-  rb.Get(value);
+  ASSERT_TRUE(rb.Get(value));
   int value2{};
-  rb.Get(value2);
+  ASSERT_TRUE(rb.Get(value2));
   EXPECT_EQ(value, 1);
   EXPECT_EQ(value2, 2);
 }
@@ -24,23 +24,43 @@ TEST(RingBufferTest, GetReturnsInOrder) {
 TEST(RingBufferTest, EmptyAfterDrained) {
   int value{};
   RingBuffer rb(2);
-  rb.Put(1);
-  rb.Get(value);
+  ASSERT_TRUE(rb.Put(1));
+  ASSERT_TRUE(rb.Get(value));
   EXPECT_TRUE(rb.Empty());
 }
 
 TEST(RingBufferTest, WrapAroundOverwritesOldest) {
   RingBuffer rb(2);
-  rb.Put(1);
-  rb.Put(2);
-  rb.Put(3);
+  ASSERT_TRUE(rb.Put(1));
+  ASSERT_TRUE(rb.Put(2));
+  EXPECT_TRUE(rb.Put(3));
   int value{};
-  rb.Get(value);
+  ASSERT_TRUE(rb.Get(value));
   EXPECT_EQ(value, 2);
+}
+
+TEST(RingBufferTest, StaysFullAfterOverwrite) {
+  RingBuffer rb(2);
+  ASSERT_TRUE(rb.Put(1));
+  ASSERT_TRUE(rb.Put(2));
+  ASSERT_TRUE(rb.Put(3));
+  EXPECT_TRUE(rb.Full());
+  EXPECT_FALSE(rb.Empty());
 }
 
 TEST(RingBufferTest, GetOnEmptyReturnsFalse) {
   int value{};
   RingBuffer rb(1);
   EXPECT_FALSE(rb.Get(value));
+}
+
+TEST(RingBufferTest, ZeroCapacityPutReturnsFalse) {
+  RingBuffer rb(0);
+  EXPECT_FALSE(rb.Put(1));
+}
+
+TEST(RingBufferTest, ZeroCapacityIsEmptyAndFull) {
+  RingBuffer rb(0);
+  EXPECT_TRUE(rb.Empty());
+  EXPECT_TRUE(rb.Full());
 }
