9ab9e44e7c11bd58f6c7dd3a821175c8ae91ecd2
Marcus441 <88486170+Marcus441@users.noreply.github.com>
2026-07-11 14:05:03 +1000

    refactor(ringbuffer): move to header only module

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,6 @@
 #include <iostream>
 
-class RingBuffer {
- private:
-  int capacity{};
-  int size{};
-  int writeIdx{};
-  int readIdx{};
-  int *val_buf;
-
- public:
-  RingBuffer(int capacity) : capacity{capacity}, val_buf{new int[capacity]{}} {}
-  ~RingBuffer() { delete[] val_buf; }
-
-  int Size() const { return size; }
-  int Capacity() const { return capacity; }
-  bool Full() const { return size == capacity; };
-  bool Empty() const { return size == 0; };
-
-  bool Put(int item) {
-    if (Full()) {
-      return false;
-    }
-    val_buf[writeIdx] = item;
-    writeIdx = (writeIdx + 1) % capacity;
-    size++;
-    return true;
-  }
-
-  bool Get(int &item) {
-    if (Empty()) {
-      return false;
-    }
-    item = val_buf[readIdx];
-    readIdx = (readIdx + 1) % capacity;
-    size--;
-    return true;
-  }
-};
+#include "ringbuffer.hpp"
 
 int main() {
   RingBuffer rb(10);
